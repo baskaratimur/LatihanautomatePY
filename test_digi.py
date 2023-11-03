2d7cbba2c6a53522ca677f73c00b7d1091c97098
baskaratimur1998 <baskaratimur2@gmail.com>
2023-11-04 00:47:33 +0700

    modified test_digi.py OOP

--- test_digi.py
+++ test_digi.py
@@ -3,26 +3,24 @@ import pytest
 import json
 import jsonpath
 
-@pytest.fixture
-def token():
-    url = "https://stag-digistar.visionplus.id/api/partnership/v1/cms/users/login"
+class TestBannerUpdate:
+    @pytest.fixture
+    def token(self):
+        url = "https://stag-digistar.visionplus.id/api/partnership/v1/cms/users/login"
+        data = {
+            "username": "innocent@mncinnovation.id",
+            "password": "!nn0c3nt"
+        }
+        headersAuth = {"Accept-Language": "id"}
+        response = requests.post(url, json=data, headers=headersAuth)
+        code = response.status_code
+        getJson = json.loads(response.text)
+        dataJson = jsonpath.jsonpath(getJson, "data.access_token")
+        yield dataJson
 
-    data = {
-        "username":"innocent@mncinnovation.id",
-        "password":"!nn0c3nt"
-    }
-
-    headersAuth = {"Accept-Langguage":"id"}
-    response = requests.post(url, json=data, headers=headersAuth)
-    code = response.status_code
-    getJson = json.loads(response.text)
-    dataJson = jsonpath.jsonpath(getJson, "data.access_token")
-    yield dataJson
-    
-
-def test_updateBanner(token):
-    url = "https://stag-digistar.visionplus.id/api/partnership/v1/contents/banner/digistar/id/1"
-    data = {
+    def test_updateBanner(self, token):
+        url = "https://stag-digistar.visionplus.id/api/partnership/v1/contents/banner/digistar/id/1"
+        data = {
             "title": "Arab Maklum Automate",
             "title_en": "Arab Maklum Automate",
             "image_url": "https://static.mncnow.id/images/series/aeb12d16/6698_r83.jpg",
@@ -31,16 +29,16 @@ def test_updateBanner(token):
             "image_popup_web": "automate",
             "position": 0,
             "content": {
-            "id": 59000,
+                "id": 59000,
                 "tags": [
                     "Drama",
                     "Religi"
                 ],
                 "type": "Series",
                 "synopsis": "Cerita tentang keluarga Arab yang tinggal di kota Jakarta, dimana sang Aba ingin mempertahankan tradisi Arab dalam keluarga dan pekerjaannya di era modernisasi. 1"
-                },
+            },
             "content_en": {
-            "id": 59000,
+                "id": 59000,
                 "tags": [
                     "Drama",
                     "Religi"
@@ -48,15 +46,14 @@ def test_updateBanner(token):
                 "type": "Series",
                 "synopsis": "Arab Maklum is a story about an Arab Family living in Jakarta, where the Aba wants to maintain Arabic traditions in his family and work in the  modernization era 1"
             }
-            }
-        
-    headersAuth = {
-        "Authorization" : "Bearer " + token[0],    
-        "Accept-Language" : "id"
-               }
-    response = requests.put(url, json=data, headers=headersAuth)
-    code = response.status_code
-    assert code == 200
-    getJson = json.loads(response.text)
-    dataTitle = jsonpath.jsonpath(getJson, "data.image_popup")
-    assert dataTitle[0] == "automate"
+        }
+        headersAuth = {
+            "Authorization": "Bearer " + token[0],
+            "Accept-Language": "id"
+        }
+        response = requests.put(url, json=data, headers=headersAuth)
+        code = response.status_code
+        assert code == 200
+        getJson = json.loads(response.text)
+        dataTitle = jsonpath.jsonpath(getJson, "data.image_popup")
+        assert dataTitle[0] == "automate"
